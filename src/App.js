ba88a42dd868c9f56a9162a1da3f0ca8d5909a82
Mithun C N <Mithun.CN@Ltts.com>
2022-03-04 21:59:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Main from "./components/screens/main/Main";
+import { Routes, Route } from "react-router-dom";
+import ForgotPassword from "./components/screens/authentication/ForgotPassword";
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      {/* <Route path="forgot" element={<ForgotPassword />} /> */}
+    </Routes>
   );
 }
 
