02bcb7d09e2c7ff26046e26ef8d503c775c54135
Mithun C N <Mithun.CN@Ltts.com>
2022-04-20 12:45:12 +0530

    removed conflict

--- src/components/screens/dashboard/DashboardMain.js
+++ src/components/screens/dashboard/DashboardMain.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
 
-<<<<<<< Updated upstream
 // import LogoNotification from "/home/user/AiKno/AiKnoWebApp/AiKnoFrontEnd_v2/AiKnoFrontend/src/assets/icons/Icon-checkmark-circle.svg"
 // import LogoAdmin from "/home/user/AiKno/AiKnoWebApp/AiKnoFrontEnd_v2/AiKnoFrontend/src/assets/icons/Icon-checkmark-circle.svg";
 // import LogoLicense from "/home/user/AiKno/AiKnoWebApp/AiKnoFrontEnd_v2/AiKnoFrontend/src/assets/icons/Icon-checkmark-circle.svg";
@@ -9,59 +8,39 @@ import LogoNotification from "..\\src\\assets\\icons\\Notification-bg.svg";
 import LogoAdmin from "..\\src\\assets\\icons\\Admin_graditi_bg.svg";
 import LogoLicense from "..\\src\\assets\\icons\\License_graditi_bg.svg";
 
-
 import axios from "axios";
 
 import * as ReactBootStrap from "react-bootstrap";
 import { AuthenticatedService } from "../../../services/api-service/AuthenticatedService";
 
-
-=======
-//import LogoNotification from "/home/user/AiKno/AiKnoWebApp/AiKnoFrontEnd_v2/AiKnoFrontend/src/assets/icons/Icon-checkmark-circle.svg";
-//..\\src\\assets\\logo\\AiKnologo.svg
-import LogoNotification from "..\\src\\assets\\icons\\notification_bg.svg";
-//import LogoAdmin from "/home/user/AiKno/AiKnoWebApp/AiKnoFrontEnd_v2/AiKnoFrontend/src/assets/icons/Icon-checkmark-circle.svg";
-import LogoAdmin from "..\\src\\assets\\icons\\admin_dashboard_bg.svg";
-import LogoLicense from "..\\src\\assets\\icons\\license_dashboard_bg.svg";
-import * as ReactBootStrap from "react-bootstrap";
-import { AuthenticatedService } from "../../../services/api-service/AuthenticatedService";
-
->>>>>>> Stashed changes
 const DashboardMain = () => {
   const authenticatedService = new AuthenticatedService();
 
   const [activityData, setActivityData] = useState([]);
 
-<<<<<<< Updated upstream
   const authService = new AuthenticatedService();
 
   const [posts, setPosts] = useState({ blogs: [] });
 
-
   useEffect(() => {
-
     const fetchPostList = async () => {
-
       const { data } = await axios(
-
         "http://localhost:8097/recentActivity/superUser/fetch/Bhanu"
         //"https://jsonplaceholder.typicode.com/users/"
-
       );
 
       setPosts({ blogs: data });
 
       console.log(data);
-
     };
 
     fetchPostList();
-
   }, [setPosts]);
 
-  return (
+  useEffect(() => {
+    getActivityList();
+  }, []);
 
-=======
   const getActivityList = () => {
     authenticatedService.recentActivity().then((res) => {
       if (res) {
@@ -71,18 +50,9 @@ const DashboardMain = () => {
     });
   };
 
-  useEffect(() => {
-    getActivityList();
-  }, []);
-
   return (
->>>>>>> Stashed changes
     <div class="container">
       <div className="welcome-tag"> Welcome to Dashboard</div> <br />
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
       <div class="row row-flex">
         <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="content colour">
@@ -120,59 +90,46 @@ const DashboardMain = () => {
         <div className="welcome-tag"> Recent Activity</div> <br /> <br />
         <br />
       </div>
-<<<<<<< Updated upstream
-
       <div>
+        <div>
+          <ReactBootStrap.Table className="tbl1">
+            <thead>
+              <tr className="title1">
+                <th>DATE</th>
 
-        <ReactBootStrap.Table>
+                <th>USERNAME</th>
 
-=======
-      <div>
-        <ReactBootStrap.Table className="tbl1">
->>>>>>> Stashed changes
-          <thead>
-            <tr className="title1">
-              <th>DATE</th>
+                <th>ACTION</th>
 
-              <th>USERNAME</th>
+                <th>EMAILID</th>
 
-              <th>ACTION</th>
+                <th>OPERATION</th>
 
-              <th>EMAILID</th>
+                <th>ON USER</th>
+              </tr>
+            </thead>
 
-              <th>OPERATION</th>
+            <tbody>
+              {activityData &&
+                activityData.map((item) => (
+                  <tr key={item.userId}>
+                    <td>{item.title}</td>
 
-              <th>ON USER</th>
-            </tr>
-          </thead>
+                    <td>{item.completed}</td>
 
-          <tbody>
-            {activityData &&
-              activityData.map((item) => (
-                <tr key={item.userId}>
-                  <td>{item.title}</td>
+                    <td>{item.action}</td>
 
-                  <td>{item.completed}</td>
+                    <td>{item.email}</td>
 
-                  <td>{item.action}</td>
+                    <td>{item.operation}</td>
 
-                  <td>{item.email}</td>
-
-                  <td>{item.operation}</td>
-
-                  <td>{item.onUser}</td>
-                </tr>
-              ))}
-          </tbody>
-        </ReactBootStrap.Table>
-<<<<<<< Updated upstream
-
+                    <td>{item.onUser}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </ReactBootStrap.Table>
+        </div>
       </div>
-
-
-=======
-      </div>
->>>>>>> Stashed changes
     </div>
   );
 };
