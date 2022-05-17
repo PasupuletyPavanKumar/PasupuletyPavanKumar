cb0db672a74b5ff19148dae95c8c2c2d3bb4c223
Mithun C N <Mithun.CN@Ltts.com>
2022-05-17 11:36:48 +0530

    added add server ui code

--- src/components/screens/dashboard/ServerManagement.js
+++ src/components/screens/dashboard/ServerManagement.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import { Modal } from "react-bootstrap";
 import { AuthenticatedService } from "../../../services/api-service/AuthenticatedService";
+import * as XLSX from "xlsx/xlsx.mjs";
 
 const ServerManagement = () => {
   const authenticatedService = new AuthenticatedService();
@@ -15,7 +16,7 @@ const ServerManagement = () => {
     setshowDeleteModal(false);
     setIsEdit(false);
     setAddAdminFields({
-      staticIp: "",
+      ServerName: "",
       ipAddress: "",
     });
     setShow(true);
@@ -27,35 +28,53 @@ const ServerManagement = () => {
 
   const inputValidators = () => {};
 
-  const [serversList, setServersList] = useState([]);
+  const [adminList, setadminList] = useState([]);
   const [addAdminFields, setAddAdminFields] = useState({
-    staticIp: "",
+    ServerName: "",
     ipAddress: "",
   });
 
-  const getServersList = () => {
-    authenticatedService.getServerList().then((res) => {
+  const exportFile = () => {
+    authenticatedService.exportFile().then((res) => {
       if (res) {
-        setServersList(res);
+        downloadToExcel(res);
+      }
+      console.log(res.headers);
+    });
+  };
+
+  const downloadToExcel = (data) => {
+    let ws = XLSX.utils.json_to_sheet(data);
+    let wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "sheet");
+    let buf = XLSX.write(wb, { bookType: "xlsx", type: "buffer" }); // generate a nodejs buffer
+    let str = XLSX.write(wb, { bookType: "xlsx", type: "binary" }); // generate a binary string in web browser
+    XLSX.writeFile(wb, `myfilename.xlsx`);
+  };
+
+  const getAdminsList = () => {
+    authenticatedService.getAdmin().then((res) => {
+      if (res) {
+        setadminList(res);
       }
       console.log(res);
     });
   };
 
   useEffect(() => {
-    getServersList();
+    getAdminsList();
   }, []);
 
   const handleInputFields = (event, field) => {
     setAddAdminFields({
       firstname:
-        field === 1 ? event.target.value.trim() : addAdminFields.staticIp,
+        field === 1 ? event.target.value.trim() : addAdminFields.ServerName,
       lastname:
         field === 2 ? event.target.value.trim() : addAdminFields.ipAddress,
     });
   };
 
-  const addServerForm = () => {
+  const adminForm = () => {
     return (
       <div className="popup">
         <button
@@ -70,20 +89,20 @@ const ServerManagement = () => {
         <div className="modal-heading">Server Details</div>
         <form className="p-3">
           <div className="row">
-            <div className="form-group col-sm-6 m-auto p-3">
+            <div className="form-group col-sm-4 m-auto p-3">
               <label for="usr" className="label-popup">
-                Static IP
+                Server Name
               </label>
               <br />
               <input
                 type="text"
                 className="input-field"
-                id="staticIp"
-                value={addAdminFields.staticIp}
+                id="ServerName"
+                value={addAdminFields.serverName}
                 onChange={(e) => handleInputFields(e, 1)}
               />
             </div>
-            <div className="form-group col-sm-6 m-auto p-3">
+            <div className="form-group col-sm-4 m-auto p-3">
               <label for="usr" className="label-popup">
                 IP Address
               </label>
@@ -94,6 +113,149 @@ const ServerManagement = () => {
                 id="ipAddress"
                 value={addAdminFields.ipAddress}
                 onChange={(e) => handleInputFields(e, 2)}
+              />
+            </div>
+            <div className="form-group col-sm-4 m-auto p-3">
+              <label for="usr" className="label-popup">
+                Location
+              </label>
+              <br />
+              {/* <input type="text" className="input-field" id="usr" /> */}
+              <div className="input-field">
+                <select
+                  name="Locations"
+                  id="location"
+                  className="select-pane"
+                  value={addAdminFields.location}
+                  onChange={(e) => handleInputFields(e, 3)}
+                >
+                  <option value="Bangalore">Bangalore</option>
+                  <option value="Chennai">Chennai</option>
+                  <option value="Mysore">Mysore</option>
+                  <option value="Pune">Pune</option>
+                </select>
+              </div>
+            </div>
+            <div className="form-group col-sm-4 m-auto p-3">
+              <label for="usr" className="label-popup">
+                Start Date
+              </label>
+              <br />
+              <input
+                type="date"
+                className="input-field"
+                id="usr"
+                value={addAdminFields.startDate}
+                onChange={(e) => handleInputFields(e, 4)}
+              />
+            </div>
+            <div className="form-group col-sm-4 m-auto p-3">
+              <label for="usr" className="label-popup">
+                Stop Date
+              </label>
+              <br />
+              <input
+                type="date"
+                className="input-field"
+                id="usr"
+                value={addAdminFields.stopDate}
+                onChange={(e) => handleInputFields(e, 5)}
+              />
+            </div>
+            <div className="form-group col-sm-4 m-auto p-2">
+              <div className="input-field">
+                <label for="usr" className="label-popup">
+                  Status
+                </label>
+                <br />
+                <select
+                  name="Status"
+                  id="status"
+                  className="select-pane"
+                  value={addAdminFields.status}
+                  onChange={(e) => handleInputFields(e, 6)}
+                >
+                  <option value="Active">Active</option>
+                  <option value="InActive">InActive</option>
+                </select>
+              </div>
+            </div>
+            <div className="form-group col-sm-4 m-auto p-3">
+              <label for="usr" className="label-popup">
+                List Of Applications
+              </label>
+              <br />
+              {/* <input type="text" className="input-field" id="usr" /> */}
+              <div className="input-field">
+                <select
+                  name="ListOfApplications"
+                  id="loa"
+                  className="select-pane"
+                  value={addAdminFields.listOfApplication}
+                  onChange={(e) => handleInputFields(e, 7)}
+                >
+                  <option value="IC">IC</option>
+                  <option value="IQM">IQM</option>
+                  <option value="ROI">ROI</option>
+                  <option value="STAMP">STAMP</option>
+                  <option value="LOGO">LOGO</option>
+                  <option value="SIGNATURE">SIGNATURE</option>
+                  <option value="TEMPLATE">TEMPLATE</option>
+                  <option value="NLP">NLP</option>
+                </select>
+              </div>
+            </div>
+            <div className="form-group col-sm-4 m-auto p-3">
+              <label for="usr" className="label-popup">
+                Server CPU
+              </label>
+              <br />
+              <input
+                type="text"
+                className="input-field"
+                id="CPU"
+                value={addAdminFields.cpu}
+                onChange={(e) => handleInputFields(e, 8)}
+              />
+            </div>
+            <div className="form-group col-sm-4 m-auto p-3">
+              <label for="usr" className="label-popup">
+                Server GPU
+              </label>
+              <br />
+              <input
+                type="text"
+                className="input-field"
+                id="gpu"
+                value={addAdminFields.GPU}
+                onChange={(e) => handleInputFields(e, 9)}
+              />
+            </div>
+            <div className="form-group col-sm-4 m-auto p-3">
+              <label for="usr" className="label-popup">
+                Server RAM
+              </label>
+              <br />
+              <input
+                type="text"
+                className="input-field"
+                id="RAM"
+                value={addAdminFields.RAM}
+                onChange={(e) => handleInputFields(e, 10)}
+              />
+            </div>
+
+            <div className="form-group col-sm-4 m-auto p-4">
+              <label for="usr" className="label-popup">
+                Server Storage
+              </label>
+              <br />
+              <input
+                type="text"
+                className="input-field"
+                id="storage"
+                value={addAdminFields.Storage}
+                onChange={(e) => handleInputFields(e, 11)}
               />
             </div>
           </div>
@@ -128,7 +290,11 @@ const ServerManagement = () => {
                 <div class="row">
                   <div className="server-title"> Server A</div>
                   <div>
-                    <button type="button" className="export-button">
+                    <button
+                      type="button"
+                      className="export-button"
+                      onClick={exportFile}
+                    >
                       Export
                     </button>
                   </div>
@@ -288,9 +454,9 @@ const ServerManagement = () => {
         show={show}
         onHide={handleClose}
         size={"md"}
-        className="bootstrap-modal"
+        className="server-modal"
       >
-        {showDeleteModal ? null : addServerForm()}
+        {showDeleteModal ? null : adminForm()}
       </Modal>
     </div>
   );
